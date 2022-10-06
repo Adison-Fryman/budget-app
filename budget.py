869454c24f3d1002639321c51106033287bbcc99
Adison <adison.fryman@gmail.com>
2022-10-06 09:53:49 -0400

    started functional code. There are still 3 errors to fix in funds, balance and transfer. Then I must work on the display which is currently commented out for testing of the functionality.

--- budget.py
+++ budget.py
@@ -1,54 +1,59 @@
 class Category:
-  def __init__(self, name, ledger, available_funds = 0):
+
+  def __init__(self, name):
     self.name = name
-    self.ledger = ledger
-    self.available_funds = available_funds
-    
+    self.ledger = list()
+    self.available_funds = 0
+
+
   def deposit(self, amount, description=''):
-    self.ledger += {"amount": amount, "description": description}
-    pass
+    self.ledger.append({'amount': amount, 'description': description})
+    self.available_funds += int(amount)
+    #no return?
+
 
   def withdraw(self, amount, description=''):
-    if #value of check_funds is > than amount
-      self.ledger += {"amount": (amount*-1), "description": description}
+    neg_amount= amount*-1
+    if self.check_funds(amount):
+      self.ledger.append({'amount': neg_amount, 'description': description})
+      self.available_funds += int(neg_amount)
       return True
     else: 
       return False
-    pass
+
 
   def get_balance(self):
-    # gets the balance from the sumation of the withraws and deposits, returns the balance numerical
-    pass
+    # gets the balance from the summation of the withdrawals and deposits in ledger, returns the balance in numerical format
+    return self.available_funds
 
-  def transfer(self,amount, source_cat, description = ''):
-    description = f'Transfer from {source_cat}'
-    if #value of check_funds is > than amount
-      self.withdraw(amount, description)
+
+  def transfer(self,amount,destination_cat):
+    if self.check_funds(amount):
+      self.withdraw(amount, description=f'Transfer to {destination_cat}')
+      destination_cat.deposit(amount, description=f'Transfer from {destination_cat}')
       return True
     else: 
       return False
     pass
   
 
-  def check_funds(self,amount)
-    #confused what is the differnce between get balance and check funds. looks to be the output
-    if get_balance>=amount
+  def check_funds(self,amount):
+    #compairs the difference between the total from the ledger and the amount provided.
+    if self.available_funds > amount:
       return True
     else:
       return False
     
-    pass
+
 
   def __repr__(self):
-    return f'A title line of 30 characters where the {self.name} of the category is centered in a line of * characters.' 
+    return f'******* {self.name} ********'
+    #placeholder for testing, supposed to be an exact string with 30 characters and self.name centered
     #also a list of the items in the ledger, each line should show the description and amaount.
     #a line with the total.
 
 
 
-  def create_spend_chart(categories):
-  
-  
 ### outside of class
 def create_spend_chart(list_cats):
   return f'string that is a bar chart'
